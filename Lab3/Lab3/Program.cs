16da6b0fad5a7c0c0f4e8b24813256b4275d9867
Jordji <37375293+goldenapple545@users.noreply.github.com>
2022-10-06 20:01:19 +0300

    Mission complete

--- Lab3/Lab3/Program.cs
+++ Lab3/Lab3/Program.cs
@@ -11,17 +11,19 @@ namespace Lab3
         public string destination;
         private int flightNumber;
         public FlightType flightType;
-        private string flightTime;
+        private int flightTime;
         private int weekday;
 
-        public Aeroflot (string d, int n, string time, int w)
+        public Aeroflot (string d, int n, int time, int w)
         {
             destination = d;
-            if (flightNumber < 0)
+            if (n < 0)
                 throw new Exception("Некорректный номер полета");
             flightNumber = n;
+            if (time < 0)
+                throw new Exception("Некорректный время");
             flightTime = time;
-            if (w > 0 && w < 8)
+            if (0 > w || w > 8)
                 throw new Exception("Некорректный день недели");
             weekday = w;
         }
@@ -31,12 +33,14 @@ namespace Lab3
             flightType = type;
         }
 
+        
+
         public int getFlightNumber()
         {
             return flightNumber;
         }
 
-        public string getFlightTime()
+        public int getFlightTime()
         {
             return flightTime;
         }
@@ -55,13 +59,63 @@ namespace Lab3
 
     class Program
     {
+        static void listDestination(Aeroflot[] mas, string destination)
+        {
+            int count = 0;
+            for (int i = 0; i < mas.Length; i++)
+            {
+                if (mas[i].destination == destination)
+                {
+                    Console.WriteLine(mas[i].info());
+                    count++;
+                }
+            }
+            if (count == 0)
+                Console.WriteLine("Список пуст");
+        }
+
+        static void listWeekday(Aeroflot[] mas, int weekday)
+        {
+            int count = 0;
+            for (int i = 0; i < mas.Length; i++)
+            {
+                if (mas[i].getWeekday() == weekday)
+                {
+                    Console.WriteLine(mas[i].info());
+                    count++;
+                }
+            }
+            if (count == 0)
+                Console.WriteLine("Список пуст");
+        }
+        static void listWeekdayTime(Aeroflot[] mas, int weekday, int time)
+        {
+            int count = 0;
+            for (int i = 0; i < mas.Length; i++)
+            {
+                if (mas[i].getWeekday() == weekday && mas[i].getFlightTime() > time)
+                {
+                    Console.WriteLine(mas[i].info());
+                    count++;
+                }   
+            }
+            if (count == 0)
+                Console.WriteLine("Список пуст");
+        }
 
         static void Main(string[] args)
         {
             Aeroflot[] masFlight = new Aeroflot[3];
-            masFlight[0] = new Aeroflot("Москва", 2, "12:31", 3);
-            masFlight[1] = new Aeroflot("Санкт-Петербург", 3, "13:35", 1);
-            masFlight[2] = new Aeroflot("Лондон", 3, "11:31", 5);
+            masFlight[0] = new Aeroflot("Москва", 2, 123, 3);
+            masFlight[1] = new Aeroflot("Санкт-Петербург", 3, 35, 1);
+            masFlight[2] = new Aeroflot("Лондон", 3, 201, 5);
+
+            Console.WriteLine("список рейсов для заданного пункта назначения");
+            listDestination(masFlight, "Лондон");
+            Console.WriteLine("список рейсов для заданного дня недели");
+            listWeekday(masFlight, 1);
+            Console.WriteLine("список рейсов для заданного дня недели, время вылета для которых больше заданного");
+            listWeekdayTime(masFlight, 3, 70);
         }
     }
 }
